0d930e37d3a9cef4324aa19450a56c5f8643c210
satwik sanand <sanandsatwik421@gmail.com>
2025-11-21 08:51:08 +0000

    initialized the repo

---
+++ src/main/java/com/github/satwiksanand/scalableUrlShortener/ScalableUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.github.satwiksanand.scalableUrlShortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalableUrlShortenerApplication.class, args);
+	}
+
+}
